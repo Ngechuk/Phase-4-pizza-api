00aa5e18d007d732f10dd97b0db6494fac578648
Kevin <kelvinngechu@gmail.com>
2023-03-20 20:42:40 +0300

    adding serializations

---
+++ app/controllers/restaurants_controller.rb
@@ -0,0 +1,30 @@
+class RestaurantsController < ApplicationController
+    rescue_from ActiveRecord::RecordNotFound, with: :render_record_not_found_response
+    
+        def index
+           restaurants = Restaurant.all 
+           render json: restaurants   
+        end 
+        
+        def show
+            restaurant = find_restaurant
+            render json: restaurant, serializer: RestaurantWithPizzasSerializer        
+        end 
+        
+        def destroy
+            restaurant = find_restaurant
+            restaurant.destroy
+            head :no_content
+        end 
+    
+        private 
+    
+        def find_restaurant 
+            Restaurant.find(params[:id])
+        end
+    
+        def render_record_not_found_response
+            render json: { error: "Restaurant not found" }, status: :not_found
+        end
+    end
+    
